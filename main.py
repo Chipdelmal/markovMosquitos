260f2a5b4977659db098d25d649de25ae8d256e1
chipdelmal <chipdelmal@gmail.com>
2019-06-19 12:41:12 -0700

    Minor update before thinking about structures (landscape).

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ import landscape as land
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Mosquito biological behaviour
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 # Create mask matrix: This matrix defines how probable is for a mosquito to
 #   move from one life stage to the next (and, as a consequence, from a site
 #   type to the next).
@@ -21,9 +20,12 @@ passMkvtest = aux.testMarkovMat(mskMat)
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Landscape
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Creates a random landscape (x,y coordinates) and calculates the distances
+#   matrix. Also assigns each coordinate a class. It would make sense to have
+#   this in a wrapper function to keep coordinates and classes in the same
+#   structure. 
 landscape = land.genURandLandscape(lo, hi, ptsNum)
 distMat = aux.distanceMat(landscape)
 pointClasses = land.genURandLandscapeClasses(classNum, ptsNum)
-
 
 print(pointClasses)
